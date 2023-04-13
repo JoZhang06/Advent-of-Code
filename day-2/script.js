64c58e63d7668c14da8ff81cd396f21cc394d430
joesang <joe.chang.1996@hotmail.com>
2023-04-13 17:02:59 -0400

    add

--- day-2/script.js
+++ day-2/script.js
@@ -1,27 +1,90 @@
-fetch('input.txt')
-    .then(response => response.text())
-    .then(data => {
-        const lines = data.trim().split('\n');
-        let result = "";
+//PART 1
 
-        for (let i = 0; i < lines.length; i += 1) {
-            const firstLetter = lines[i];
-            const secondLetter = lines[i + 1];
-            // Aquí puedes hacer lo que necesites con las letras
-
-            // Agregar el resultado al string de resultados
-            result += `${firstLetter}${secondLetter}, `;
-        }
-
-        // Mostrar el resultado
-        document.getElementById("resultado").innerHTML = result;
-        console.log("El resultado es: " + result);
-
-        // Ejemplo de if/else
-        if (result.includes("C,Y")) {
-            console.log("Se encontró la secuencia 'C,Y'");
-        } else {
-            console.log("No se encontró la secuencia 'C,Y'");
-        }
-    })
-    .catch(error => console.error(error));
+const summonPuzzleInput = async (day) => {
+    const response = await fetch(`https://adventofcode.com/2022/day/2/input`);
+    const responseText = await response.text();
+    return responseText.trim();
+  };
+  
+  /* 
+  A is rock
+  B is paper
+  C is scissors
+  
+  X is rock (1 point)
+  Y is paper (2 points)
+  Z is scissors (3 points)
+  */
+  
+  const sumReducer = (sum, num) => sum + num;
+  
+  const puzzleInput = await summonPuzzleInput(2);
+  
+  const gameValues = {
+    A: { X: 3, Y: 6, Z: 0 },
+    B: { X: 0, Y: 3, Z: 6 },
+    C: { X: 6, Y: 0, Z: 3 },
+  };
+  
+  const moreValues = { X: 1, Y: 2, Z: 3 };
+  
+  const getTheScore = (game) => {
+    const [opponentMove, yourMove] = game.split(" ");
+    //get the game score (win, lose, draw)
+    const gameScore = gameValues[opponentMove][yourMove];
+  
+    // console.log(gameScore + ", " + yourMove + ", " + opponentMove);
+    //get the moce score
+    const moveScore = moreValues[yourMove];
+  
+    //add them up
+    return gameScore + moveScore;
+  };
+  
+  //get the score of all the games
+  const games = puzzleInput.split("\n");
+  const scores = games.map(getTheScore);
+  
+  //add them all up
+  const totalScore = scores.reduce((sum, num) => sum + num, 0);
+  
+  console.log(totalScore);
+  
+  //PART 2
+  /*
+  X is lose
+  Y is draw
+  Z is win
+  */
+  
+  const requiredMoves = {
+    A: { X: "S", Y: "R", Z: "P" },
+    B: { X: "R", Y: "P", Z: "S" },
+    C: { X: "P", Y: "S", Z: "R" },
+  };
+  
+  const actualMoveValues = { R: 1, P: 2, S: 3 };
+  
+  const actualGameValues = { X: 0, Y: 3, Z: 6 };
+  
+  const getTheActualScore = (game) => {
+    const [opponentMove, gameOutcome] = game.split(" ");
+  
+    //find which move you need to play
+    const yourMove = requiredMoves[opponentMove][gameOutcome];
+  
+    //get the game score
+    const gameScore = actualGameValues[gameOutcome];
+  
+    //get the move score
+    const moveScore = actualMoveValues[yourMove];
+  
+    //add them up
+    return gameScore + moveScore;
+  };
+  
+  const actualScores = games.map(getTheActualScore);
+  const actualTotalScore = actualScores.reduce(sumReducer, 0);
+  
+  console.log(actualTotalScore);
+  
