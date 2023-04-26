a1d84018e3e7ec461cf14eedbfbd7392e7b30125
joesang <joe.chang.1996@hotmail.com>
2023-04-26 17:36:26 -0400

    day 10

--- day-10/script.js
+++ day-10/script.js
@@ -1,5 +1,4 @@
-const commands = ["noop", "noop", "addx 5", "addx 3", "noop", "addx 14", "addx -12", "noop", "addx 5", "addx 1", "noop", "addx 19", "addx -15", "noop", "noop", "noop", "addx 7", "addx -1", "addx 4", "noop", "noop", "addx 5", "addx 1", "addx -38", "noop", "addx 21", "addx -18", "addx 2", "addx 2", "noop", "addx 3", "addx 5", "addx -6", "addx 11", "noop", "addx 2", "addx 19", "addx -18", "noop", "addx 8", "addx -3", "addx 2", "addx 5", "addx 2", "addx 3", "addx -2", "addx -38", "noop", "addx 3", "addx 4", "addx 5", "noop", "addx -2", "addx 5", "addx -8", "addx 12", "addx 3", "addx -2", "addx 5", "addx 11", "addx -31", "addx 23", "addx 4", "noop", "noop", "addx 5", "addx 3", "addx -2", "addx -37", "addx 1", "addx 5", "addx 2", "addx 12", "addx -10", "addx 3", "addx 4", "addx -2", "noop", "addx 6", "addx 1", "noop", "noop", "noop", "addx -2", "addx 7", "addx 2", "noop", "addx 3", "addx 3", "addx 1", "noop", "addx -37", "addx 2", "addx 5", "addx 2", "addx 32", "addx -31", "addx 5", "addx 2", "addx 9", "addx 9", "addx -15", "noop", "addx 3", "addx 2", "addx 5", "addx 2", "addx 3", "addx -2", "addx 2", "addx 2", "addx -37", "addx 5", "addx -2", "addx 2", "addx 5", "addx 2", "addx 16", "addx -15", "addx 4", "noop", "addx 1", "addx 2", "noop", "addx 3", "addx 5", "addx -1", "addx 5", "noop", "noop", "noop", "noop", "addx 3", "addx 5", "addx -16", "noop"];
-
+const commands = ["noop", "addx 22", "addx -17", "addx 1", "addx 4", "addx 17", "addx -16", "addx 4", "addx 1", "addx 21", "addx -17", "addx -10", "noop", "addx 17", "addx -1", "addx 5", "addx -1", "noop", "addx 4", "addx 1", "noop", "addx -37", "addx 5", "addx 27", "addx -22", "addx -2", "addx 2", "addx 5", "addx 2", "addx 5", "noop", "noop", "addx -2", "addx 5", "addx 16", "addx -11", "addx -2", "addx 2", "addx 5", "addx 2", "addx -8", "addx 9", "addx -38", "addx 5", "addx 20", "addx -16", "addx 8", "addx -5", "addx 1", "addx 4", "noop", "noop", "addx 5", "addx -2", "noop", "noop", "addx 18", "noop", "addx -8", "addx 2", "addx 7", "addx -2", "noop", "noop", "noop", "noop", "noop", "addx -35", "noop", "addx 32", "addx -26", "addx 12", "addx -8", "addx 3", "noop", "addx 2", "addx 16", "addx -24", "addx 11", "addx 3", "addx -17", "addx 17", "addx 5", "addx 2", "addx -15", "addx 22", "addx 3", "noop", "addx -40", "noop", "addx 2", "noop", "addx 3", "addx 13", "addx -6", "addx 10", "addx -9", "addx 2", "addx 22", "addx -15", "addx 8", "addx -7", "addx 2", "addx 5", "addx 2", "addx -32", "addx 33", "addx 2", "addx 5", "addx -39", "addx -1", "addx 3", "addx 4", "addx 1", "addx 4", "addx 21", "addx -20", "addx 2", "addx 12", "addx -4", "noop", "noop", "noop", "noop", "noop", "addx 4", "noop", "noop", "noop", "addx 6", "addx -27", "addx 31", "noop", "noop", "noop", "noop", "noop"];
 function getPower(cycle, x) {
   if ((cycle - 20) % 40 === 0) {
     return (cycle * x);
@@ -56,8 +55,6 @@ function solve(part) {
     screen.forEach(row => console.log(row.join("")));
   }
 }
-// 16880
 solve("part1");
 
-//RKAZAJBR
 solve("part2");
